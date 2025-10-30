c55e09ec3390bd8144cf5e0c7af8fedec3d28124
alexsander <golovanovbiba@gmail.com>
2025-10-30 18:45:38 +0300

    premens

--- src/index.php
+++ src/index.php
@@ -1,3 +1,45 @@
 <?php
-echo "hello world";
+/*echo "hello world";
+echo 67;
+$a=997;
+$b=10;
+$c=$a+$b;
+echo $c;
+$Q=10;
+$x=50;
+$m=$Q*$x;
+echo $m;
+$n=4;
+$k=8;
+$v=$n/$k;
+echo $v*/
+
+/*$b=10;
+if ($b>20) {
+  echo $b;
+} else {
+    echo "super duper lavalava chips";
+}*/
+
+/*$age=27;
+if ($age<18) {
+    echo 'kypi chichis i zaxpdi(shutka net)';
+}
+elseif (($age>=18) && ($age<=23)) {
+    echo "moldec no seravno kypi chichis";
+}
+else {
+    echo "ny ti i vzrosli esli viskachil itit text";
+}*/
+
+$month=10;
+if (($month==12) || ($month==1) || ($month==2))
+ {echo "zima";
+} elseif (($month==3) || ($month==4) || ($month==5)) {
+    echo "vesna";
+} elseif (($month==6) || ($month==7) || ($month==8)) {
+    echo "summer";
+} elseif (($month==9) || ($month==10) || ($month==11)) {
+    echo "arsen";}
+
 ?>
