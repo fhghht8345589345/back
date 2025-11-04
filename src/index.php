b51b5f4a77531a25d3a9310580644db932a09b49
alexsander <golovanovbiba@gmail.com>
2025-11-04 18:45:36 +0300

    cikl

--- src/index.php
+++ src/index.php
@@ -32,7 +32,7 @@ else {
     echo "ny ti i vzrosli esli viskachil itit text";
 }*/
 
-$month=10;
+/*#$month=10;
 if (($month==12) || ($month==1) || ($month==2))
  {echo "zima";
 } elseif (($month==3) || ($month==4) || ($month==5)) {
@@ -42,4 +42,34 @@ if (($month==12) || ($month==1) || ($month==2))
 } elseif (($month==9) || ($month==10) || ($month==11)) {
     echo "arsen";}
 
+$z=0;
+while ($z<=10){
+echo $z ;
+$z++;
+}
+$J=100;
+while ($J<=200) {
+$J++;
+ echo $J ;
+} */
+
+
+/*for($PM=0; $PM<=10; $PM++){ echo $PM ;}
+ 
+for($PM=100; $PM<=200; $PM++){ echo $PM ;}
+
+for ($TB=1; $TB<=10; $TB++) {
+    for($GL=1; $GL<=10; $GL++) {
+        for($WO=1; $WO<=10; $WO++)
+        echo $TB*$GL*$WO . "<br>";
+    }
+} */
+ 
+/*$KP=[67,89,90,34,56,57,80,12,36,55]; 
+foreach ($KP as $item) {
+    echo $item ."<br>" ;
+}*/
+
+$TO=["lorem","impus","notcoin","altcoin","fpibank","bitcoin","xamstrcoin","meloncoin","bybit"];
+echo $TO[2] . " " . $TO[7] . " " . $TO[8];
 ?>
