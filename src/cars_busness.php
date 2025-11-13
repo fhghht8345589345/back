ae925d030cc4477c0bcf99fe04be9ae3489ae435
alexsander <golovanovbiba@gmail.com>
2025-11-13 18:48:44 +0300

    apdate

--- src/cars_busness.php
+++ src/cars_busness.php
@@ -5,7 +5,7 @@ $dbname = 'myapp';
 $username = 'root';
 $password = 'rootpassword';
 $port = 3306;
-
+$host = 'db';
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -13,19 +13,20 @@ $options = [
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
-$pdo = new PDO($dsn, $username, $password, $options);подключения
+$pdo = new PDO($dsn, $username, $password, $options);
+$stmt1=$pdo->prepare  ("insert into `cars_business` (name,price,brend) values (?,?,?)");
+$stmt1->execute(["a4",1300000,"audi"]);
 
-
-
-$stmt=$pdo->prepare ("SELECT * FROM `narrko shops`");
+$stmt=$pdo->prepare ("SELECT*FROM `cars_business`");
 $stmt->execute();
 $data=$stmt->fetchAll();
-print_r($data);
+var_dump($data);
+
 {
     echo '<pre>';
-    print_r($data);
+    var_dump($data);
     echo '</pre>';
 }
 
-
-?>
+$stmt2=$pdo->prepare("delete from `cars_business` where id=?");
+$stmt2->execute([13]);
